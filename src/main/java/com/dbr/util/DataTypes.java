03c821d2f9892a299bc876d2e3e56872dd957550
dominik.bruhn <dominik.bruhn@de.rhenus.com>
2020-01-27 08:40:58 +0100

    huhu

---
+++ src/main/java/com/dbr/util/DataTypes.java
@@ -0,0 +1,24 @@
+package com.dbr.util;
+
+public class DataTypes {
+
+
+    public static final String JAVA_TYPE_BYTE_ARRAY = "byte[]";
+    public static final String TYPE_STRING = "String";
+    public static final String TYPE_INTEGER = "Integer";
+    public static final String TYPE_BOOLEAN = "Boolean";
+    public static final String TYPE_BIG_DECIMAL = "BigDecimal";
+    public static final String TYPE_LONG = "Long";
+    public static final String TYPE_FLOAT = "Float";
+    public static final String TYPE_SHORT = "Short";
+    public static final String TYPE_DOUBLE = "Double";
+    public static final String TYPE_CHAR = "Char";
+    public static final String TYPE_DATE = "Date";
+    public static final String TYPE_DATE_ISO8601 = "TYPE_DATE_ISO8601";
+    public static final String TYPE_ARRAY_STRING = "String[]";
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/com/dbr/util/DateUtil.java
@@ -0,0 +1,10 @@
+package com.dbr.util;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+public class DateUtil {
+
+    public static DateFormat DATE_FORMAT_ISO8601 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+
+}
