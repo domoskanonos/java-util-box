e89dc10aadea78f98199525e89d91df59719555c
dominik.bruhn <dominik.bruhn@de.rhenus.com>
2020-01-31 15:24:03 +0100

    huhu

--- src/main/java/com/dbr/util/resource/ResourceUtil.java
+++ src/main/java/com/dbr/util/resource/ResourceUtil.java
@@ -1,5 +1,8 @@
 package com.dbr.util.resource;
 
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.FileUtils;
+
 import java.io.*;
 import java.nio.charset.Charset;
 import java.util.stream.Collectors;
@@ -73,6 +76,40 @@ public class ResourceUtil {
             return br.lines().collect(Collectors.joining(System.lineSeparator()));
         }
     }
+
+
+    public static String asDataUri(File file) throws IOException {
+        String retVal = "data:";
+        String filename = file.getName();
+        String fileEnding = filename.substring(filename.lastIndexOf(".") + 1, filename.length());
+        switch (fileEnding.toUpperCase()) {
+            case "JPG":
+            case "PNG":
+                retVal += "image/";
+                break;
+            default:
+                retVal += "application/";
+                break;
+        }
+        retVal += fileEnding + ";base64,";
+        try {
+            retVal += Base64.encodeBase64String(FileUtils.readFileToByteArray(file));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return retVal;
+    }
+
+    public static byte[] dataUriToByteArray(String dataUri) {
+        if (dataUri.contains("base64")) {
+            String base64 = dataUri.substring(dataUri.indexOf("base64") + 7, dataUri.length());
+            return Base64.decodeBase64(base64);
+        } else {
+            String data = dataUri.substring(dataUri.indexOf(",") + 1, dataUri.length());
+            return data.getBytes();
+        }
+    }
+
 
 
 }
