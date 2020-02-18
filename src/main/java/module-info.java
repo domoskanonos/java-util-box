ad8373a07fc14103130666fc224928c01d4ab2f7
dominik.bruhn <dominikbruhn@googlemail.com>
2020-02-18 21:38:10 +0100

    Modularisiert

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,10 @@
 module com.dbr.util {
     requires commons.lang;
-    requires commons.io;
+    requires org.apache.commons.codec;
+    requires org.apache.commons.io;
     requires java.logging;
     requires java.desktop;
     requires guava;
-    requires org.apache.commons.codec;
     exports com.dbr.util;
     exports com.dbr.util.resource;
 }
